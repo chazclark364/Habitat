831399b73e1cca79c513c841e8a566fbaeb978ac
Max Minard <mdminard@iastate.edu>
2018-09-10 00:55:46 -0500

    Initial Practice Commit

--- Experiments/MaxMinard_Experiments/Habitat/ViewController.swift
+++ Experiments/MaxMinard_Experiments/Habitat/ViewController.swift
@@ -10,19 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SignInLabel: UILabel!
     
-    @IBOutlet weak var UITextField: UITextField!
-    @IBOutlet weak var UILabel: UILabel!
+    @IBOutlet weak var UsernameLabel: UILabel!
+    @IBOutlet weak var UsernameText: UITextField!
+
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var PasswordLabel: UILabel!
     
+    @IBOutlet weak var Message: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func UIButton(_ sender: UIButton) {
+    @IBAction func Login(_ sender: UIButton) {
         
-        UILabel.text = UITextField.text;
+        Message.text = "Please Enter Login Credentials";
+        
+        if(UsernameText.text == "maxminard" &&
+            PasswordText.text == "coms309"){
+            performSegue(withIdentifier: "login", sender: self);
+        }
+        else{
+            Message.text = "Incorrect Username or Password";
+        }
+        
         
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
