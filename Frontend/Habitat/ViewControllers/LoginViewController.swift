1dec999ee88c506f12b594e5ef5b95a351355eeb
Travis Stanger <>
2018-09-26 20:21:52 -0500

    Added main startup screen/login screen/sign up screen

---
+++ Frontend/Habitat/ViewControllers/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Habitat
+//
+//  Created by Travis Stanger on 9/26/18.
+//  Copyright © 2018 PP1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
