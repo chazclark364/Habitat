230ef180d790c548c8a1778db51cd2f995ea970f
Chaz Clark <chadclark99@gmail.com>
2018-09-29 12:26:16 -0500

    Installed third party network dependency 'AlamoFire'

---
+++ Frontend/Habitat/NetworkingAPIs/HabitatAPI.swift
@@ -0,0 +1,28 @@
+//
+//  HabitatAPI.swift
+//  Habitat
+//
+//  Created by Chaz Clark on 9/29/18.
+//  Copyright © 2018 PP1. All rights reserved.
+//
+
+import Foundation
+import CFNetwork
+import Alamofire
+
+
+
+/**
+ The purpose of this file is to handle all networking task.
+ 
+ Coding Style:
+ 
+ *This file will be organzied by object. Every object will have an API
+ call associated witht that specific object.
+ 
+ *Every API call needs to be called on a seperate thread:
+ 
+ */
+class HabitiatAPI {
+    
+}
