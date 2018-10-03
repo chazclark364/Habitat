47f038f4fbc9945e23a8ae06c32ea08b4526b08d
Matthew Martin <mfmartin@iastate.edu>
2018-10-02 22:33:54 -0500

    Update Controllers

---
+++ Backend/src/spring-boot/src/main/java/org/springframework/landlord/LandlordController.java
@@ -0,0 +1,11 @@
+package org.springframework.landlord;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class LandlordController{
+
+}
---
+++ Backend/src/spring-boot/src/main/java/org/springframework/tenant/TenantController.java
@@ -0,0 +1,11 @@
+package org.springframework.tenant;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class TenantController{
+
+}
