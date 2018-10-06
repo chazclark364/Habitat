f00b75f3eaab4b53c8d544fd8cd30bbd74ac3598
Travis Stanger <>
2018-10-05 21:13:25 -0500

    - Created profile editor controller 	+ WIP
    - Created lease controller
    	+ Contains upload button as well as back button
    	+ Large box for displaying lease
    	+ WIP
    
    - Modified profile controller
    	+ Added header with icon, first name, last name
    	+ Edit and sign out buttons in header
    	+ Added main box with other user information
    	+ Main box includes a button to view/upload lease
    
    - Removed testing button

--- Frontend/Habitat/ViewControllers/ProfileViewController.swift
+++ Frontend/Habitat/ViewControllers/ProfileViewController.swift
@@ -12,9 +12,18 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var backgroundTop: UIView!
+    @IBOutlet weak var backgroundBottom: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backgroundBottom.layer.cornerRadius = 24
+        backgroundBottom.clipsToBounds = true
+        backgroundTop.layer.cornerRadius = 24
+        backgroundTop.clipsToBounds = true
+        profileImage.layer.cornerRadius = 24
+        profileImage.clipsToBounds = true
     }
     
     override func didReceiveMemoryWarning() {
