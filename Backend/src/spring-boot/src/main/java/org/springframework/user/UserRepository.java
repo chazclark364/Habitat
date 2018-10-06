e0f06776d39cf2cd72b39233107bc51c0f941b89
Matthew Martin <mfmartin@iastate.edu>
2018-10-06 17:15:58 -0500

    Clean for Demo2

--- Backend/src/spring-boot/src/main/java/org/springframework/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.user;
-import org.springframework.data.repository.Repository;
-
-public interface UserRepository extends Repository<User, Integer>{
-
-    public void save(User user);
-}
