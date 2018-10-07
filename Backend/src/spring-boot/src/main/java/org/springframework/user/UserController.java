7fb7bfb5cce6dad25d5a89b903eeaa9f2d4b42eb
Matthew Martin <mfmartin@iastate.edu>
2018-10-07 11:56:48 -0500

    WIP: Login Page

---
+++ Backend/src/spring-boot/src/main/java/org/springframework/user/UserController.java
@@ -0,0 +1,50 @@
+package org.springframework.user;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import io.micrometer.core.instrument.util.MediaType;
+
+
+public class UserController {
+
+    @Autowired
+    private UserRepository users;
+
+    public UserController(UserRepository user){
+        this.users = user;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/users/new", consumes = MediaType.APPLICATION_JSON)
+    public String createUser(@RequestBody User user){
+
+        this.users.save(user);
+        return "User created";
+
+    }
+
+    @RequestMapping(path = "/users/{id_users}", method = RequestMethod.GET)
+    public @ResponseBody User getUserProfile(@PathVariable("id_users") Integer id_users, Model model){
+        User user = this.users.findByID(id_users);
+        model.addAttribute(user);
+        return user;
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON)
+    public @ResponseBody User getUserFromLogin(@RequestBody User user){
+        User loginAttempt = this.users.findByEmail(user.getEmail());
+        if(user.getPassword() == loginAttempt.getPassword()){
+            return loginAttempt;
+        }else{
+            return null;
+        }
+    }
+
+}
