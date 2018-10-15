25760d5a6a38bf85661053ab1f8e312f8aa8ff13
Matthew Martin <mfmartin@iastate.edu>
2018-10-15 15:45:03 -0500

    Spring: Create and Get Request

--- Backend/src/spring-boot/src/main/java/com/example/demo/RequestController.java
+++ Backend/src/spring-boot/src/main/java/com/example/demo/RequestController.java
@@ -1,4 +1,31 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import io.micrometer.core.instrument.util.MediaType;
+
+@RestController
 public class RequestController {
+
+    @Autowired
+    private RequestRepository request;
+
+    public RequestController(RequestRepository request){
+        this.request = request;
+    }
+    
+    @RequestMapping(path = "/request/{id_request}", method = RequestMethod.GET)
+    public @ResponseBody Request getRequest(@PathVariable("id_request") Integer id_request, Model model){
+    	Request request = this.request.findRequestByID(id_request);
+        model.addAttribute(request);
+        return request;
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, path = "/request/new", consumes = MediaType.APPLICATION_JSON)
+    public @ResponseBody Request createRequest(@RequestBody Request request){
+    	this.request.save(request);
+        return request;
+    }
+    
 }
--- Backend/src/spring-boot/src/main/java/com/example/demo/RequestRepository.java
+++ Backend/src/spring-boot/src/main/java/com/example/demo/RequestRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface RequestRepository {
+public interface RequestRepository extends Repository<Request, Integer>{
     public void save(Request request);
 
     @Query("select r from Request r where r.id_request = :id_request")
     @Transactional(readOnly = true)
-    public User findByID(@Param("id_request") Integer id_request);
+    public Request findRequestByID(@Param("id_request") Integer id_request);
 
 }
