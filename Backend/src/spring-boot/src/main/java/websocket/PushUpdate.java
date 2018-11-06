2f67f08d7567e7f276f1dbb74127fb0bee71a220
Matthew Martin <mfmartin@iastate.edu>
2018-11-05 22:19:58 -0600

    Add Method for sending notification to proper users of request

--- Backend/src/spring-boot/src/main/java/websocket/PushUpdate.java
+++ Backend/src/spring-boot/src/main/java/websocket/PushUpdate.java
@@ -7,16 +7,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
 
 /*
  *   Class that is called by the MYSQL database to call the method to update the
  *   needed users about an update in the MYSQL database.
  */
-@WebServlet("/push-update")
+@WebServlet("/push-update/{requestID}")
 public class PushUpdate extends HttpServlet{
 	
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
-		WebSocketServer.sendRequestUpdate(0);//Send update from DB
+	
+	/*
+	 *   Method is called by the MYSQL Database.
+	 *   Gets the associated requestID that has changed in the database
+	 *   Uses the WebSocketServer Send Request to send notification to proper users
+	 */
+	//@Override
+	protected void doGet(@PathParam("requestID") Integer associatedID, HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		WebSocketServer.sendRequestUpdate(associatedID);
 	}
 }
--- Backend/src/spring-boot/src/main/java/websocket/WebSocketServer.java
+++ Backend/src/spring-boot/src/main/java/websocket/WebSocketServer.java
@@ -17,28 +17,36 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.example.demo.RequestRepository;
+import com.example.demo.Request;
 
 
 /*Going to need to switch this from messaging to notifications*/
-@ServerEndpoint("/websocket/{username}")
+@ServerEndpoint("/websocket/{id_users}")
 @Component
 public class WebSocketServer {
-	private static Map<Session, String> sessionUsernameMap = new HashMap<>();
-    private static Map<String, Session> usernameSessionMap = new HashMap<>();
-	
+	private static Map<Session, String> sessionUsernameMap = new HashMap<>(); //Session is Key, UserID is value
+    private static Map<String, Session> usernameSessionMap = new HashMap<>(); //UserID is Key, Session is value
+    private final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
+    
+    @Autowired
+    private static RequestRepository requests;
+    
 	@OnOpen
-	public void onOpen(Session session, @PathParam("username") String username) throws IOException{
-		sessionUsernameMap.put(session, username);
-        usernameSessionMap.put(username, session);
-
+	public void onOpen(Session session, @PathParam("id_users") String id_users) throws IOException{
+		sessionUsernameMap.put(session, id_users);
+        usernameSessionMap.put(id_users, session);
+        logger.info(id_users);
 	}
 	
 	@OnClose
 	public void onClose(Session session) throws IOException{
-		String username = sessionUsernameMap.get(session);
+		String id_users = sessionUsernameMap.get(session);
     	sessionUsernameMap.remove(session);
-    	usernameSessionMap.remove(username);
+    	usernameSessionMap.remove(id_users);
 	}
 	
 	/*
@@ -47,12 +55,84 @@ public class WebSocketServer {
 	 *   in the MYSQL database.
 	 */
 	public static void sendRequestUpdate(Integer assoiciatedID){
+		/*
+		 *   The request that has changed from the database
+		 */
+		Request requestChanged = requests.findRequestByID(assoiciatedID);
+		
+		/*
+		 *   The 2 different users to notify on an update to a request
+		 */
+		Integer notify1 = null;
+		Integer notify2 = null;
+		
+		/*
+		 *    Title of request that has changed
+		 */
+		String title = requestChanged.getTitle();
+		
+		
+		/*
+		 *   Get the 3 different types of users from the request that has changed
+		 */
+		Integer lastUpdated = requestChanged.getLastUpdated();
+		Integer requestee = requestChanged.getRequestee();
+		Integer landlord = requestChanged.getLandlord();
+		Integer worker = requestChanged.getWorker();
+		
+		/*
+		 *   Determine the needed users to notify
+		 */
+		if(lastUpdated == requestee){
+			/*
+			 *   If the last update was the requestee and the landlord and requestee are
+			 *   the same user, only alert the worker.  Else notify both the landlord and 
+			 *   worker
+			 */
+			if(requestee == landlord){ 
+				notify1 = worker;
+			}else{
+				notify1 = landlord;
+				notify2 = worker;
+			}
+		}else if(lastUpdated == landlord){
+			/*
+			 *   If the last update was by the landlord, and the landlord and requestee are
+			 *   the same user, only alert the worker.  Else notify both the requestee and 
+			 *   worker
+			 */
+			if(requestee == landlord){
+				notify1 = worker;
+			}else{
+				notify1 = requestee;
+				notify2 = worker;
+			}
+		}else if(lastUpdated == worker){
+			/*
+			 *    If the last update was the worker and the requestee and landlord are 
+			 *    the same, only notify the landlord.  Else notify both the requestee 
+			 *    and landlord
+			 */
+			if(requestee == landlord){
+				notify1 = landlord;
+			}else{
+				notify1 = requestee;
+				notify2 = landlord;
+			}
+		}
+		
+		/*
+		 *   Send notification to needed users with an open session
+		 */
 		synchronized (usernameSessionMap) {
-			/*for (Map maps : usernameSessionMap) {
-                if (maps.isOpen()) {//Change to user ID stuff
-                    
-                }
-            }*/
+			//Send notification to 1st person
+			if(notify1 != null && usernameSessionMap.get(notify1).isOpen()){
+				usernameSessionMap.get(notify1).getAsyncRemote().sendText(title + " has been updated");
+			}
+			//Send notification to 2nd person
+			if(notify2 != null && usernameSessionMap.get(notify2).isOpen()){
+				usernameSessionMap.get(notify2).getAsyncRemote().sendText(title + " has been updated");
+			}
 		}
 	}
 	
