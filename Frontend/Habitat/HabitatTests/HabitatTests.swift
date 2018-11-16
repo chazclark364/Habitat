df552dbb8ec05e5f08481dddbbe8f67ba612f897
Chaz Clark <chadclark99@gmail.com>
2018-11-16 15:59:16 -0600

    Included Main Storyboard

--- Frontend/Habitat/HabitatTests/HabitatTests.swift
+++ Frontend/Habitat/HabitatTests/HabitatTests.swift
@@ -12,13 +12,13 @@ import XCTest
 class HabitatTests: XCTestCase {
     
     override func setUp() {
-//        super.setUp()
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-//        super.tearDown()
+        super.tearDown()
     }
     
     func testExample() {
@@ -28,9 +28,9 @@ class HabitatTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
     
 }
