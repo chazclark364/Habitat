8433c03b0888d5f726a564ad0a94ab682878004e
Matthew Martin <mfmartin@iastate.edu>
2018-11-27 16:09:36 -0600

    Fix property repository issues with old layout

--- Backend/src/spring-boot/src/main/java/com/example/demo/PropertyController.java
+++ Backend/src/spring-boot/src/main/java/com/example/demo/PropertyController.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import io.micrometer.core.instrument.util.MediaType;
+import java.util.Collection;
 
 @RestController
 class PropertyController{
--- Backend/src/spring-boot/src/main/java/com/example/demo/PropertyRepository.java
+++ Backend/src/spring-boot/src/main/java/com/example/demo/PropertyRepository.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Collection;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface PropertyRepository extends CrudRepository<Property, Integer>{
-    Collection<Property> findall();
-    Collection<Property> findById(@Param("id_property") int id);
+public interface PropertyRepository extends Repository<Property, Integer>{
     Property save(Property property);
+
 }
